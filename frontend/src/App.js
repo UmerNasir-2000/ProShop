f10290cbdc49c86339fff85c4fc4023d5b9187c3
UmerNasir-2000 <umer.nas@outlook.com>
2021-12-28 19:52:46 +0500

    Initial React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Container>
         <main className="py-3">
-          <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </main>
       </Container>
       <Footer />
-    </>
+    </Router>
   );
 };
 
